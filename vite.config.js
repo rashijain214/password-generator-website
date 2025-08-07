8b70493eb656e5ce3bff02e6eb10177b4f0ccae5
rashi jain <jainrashi214@gmail.com>
2025-08-07 23:03:12 +0530

    Update base path for correct repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/passwordgen-website/',
+  base: '/password-generator-website/',
   build: {
     outDir: 'dist',
   },
